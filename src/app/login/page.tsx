ad6554e31468da6c132844b71ef5ac752338c9f7
BeckerMM <matheus06jgs@gmail.com>
2025-09-17 22:53:19 -0300

    Created the main folders

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+    return (
+        <div className="h-screen w-screen bg-gray-100 flex justify-center items-center" >
+            <div className="bg-white p-8 rounded-xl shadow-md w-96 h-96">
+
+            </div>
+
+        </div>
+    )  
+}
